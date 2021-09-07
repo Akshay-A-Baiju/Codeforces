d2ca013d8ba05319afe8be66f8bdec378cd25146
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-09-08 00:22:06 +0530

    Update C. Closest to the Right.cpp
    #lower bound

--- ITMO/Binary Search/Step 1/C. Closest to the Right.cpp
+++ ITMO/Binary Search/Step 1/C. Closest to the Right.cpp
@@ -1,6 +1,6 @@
 // Author : Akshay A Baiju
-// Problem link : https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/A
-// Problem tags : lower_bound
+// Problem link : https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/C
+// Problem tags : lower bound
 // TC : O(k*logn)
 // SC : O(n)
 
@@ -12,7 +12,9 @@ using namespace std;
 #define vll                 vector <ll>
 #define vvll                vector <vll>
 #define pii                 pair <int,int>
+#define ppi                 pair <int,pii>
 #define pll                 pair <ll,ll>
+#define ppl                 pair <ll,pll>
 #define ms(a,x)             memset (a,x,sizeof(a))
 #define rep(i,a,n)          for (int i=a;i<n;i++)
 #define rrep(i,n,a)         for (int i=n;i>=a;i--)
@@ -31,17 +33,32 @@ using namespace std;
 const int M = 1e9+7;
 
 void solve()
-{    
+{  
     ll n,k;
     cin>>n>>k;
     vll v(n);
     scan_vector(v,n);
+    //lower bound of x
     while (k--)
     {
         ll x;
         cin>>x;
-        ll index=lower_bound(all(v),x)-v.begin()+1;
-        cout<<index<<endl;
+        //lower bound of x
+        ll lo=0,hi=n-1;
+        while (hi-lo>1)
+        {
+            ll mid=(hi+lo)/2;
+            if (v[mid]<x)
+                lo=mid+1;
+            else
+                hi=mid;
+        }
+        if (v[lo]>=x)
+            cout<<lo+1<<endl;
+        else if (v[hi]>=x)
+            cout<<hi+1<<endl;
+        else        //no lower bound exists
+            cout<<n+1<<endl;
     }
 }
 
@@ -56,6 +73,6 @@ int main()
         solve();
         cout<<endl;
     }
-    cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    //cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
     return 0;
 }
