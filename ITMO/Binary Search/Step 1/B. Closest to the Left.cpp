c191a4a30c0f51b2364d71b52a0fcc40cf45fe3a
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-09-08 00:20:53 +0530

    Update B. Closest to the Left.cpp
    #upper bound

--- ITMO/Binary Search/Step 1/B. Closest to the Left.cpp
+++ ITMO/Binary Search/Step 1/B. Closest to the Left.cpp
@@ -1,6 +1,6 @@
 // Author : Akshay A Baiju
 // Problem link : https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/B
-// Problem tags : lower_bound
+// Problem tags : upper_bound
 // TC : O(k*logn)
 // SC : O(n)
 
@@ -12,7 +12,9 @@ using namespace std;
 #define vll                 vector <ll>
 #define vvll                vector <vll>
 #define pii                 pair <int,int>
+#define ppi                 pair <int,pii>
 #define pll                 pair <ll,ll>
+#define ppl                 pair <ll,pll>
 #define ms(a,x)             memset (a,x,sizeof(a))
 #define rep(i,a,n)          for (int i=a;i<n;i++)
 #define rrep(i,n,a)         for (int i=n;i>=a;i--)
@@ -31,18 +33,33 @@ using namespace std;
 const int M = 1e9+7;
 
 void solve()
-{    
+{  
     ll n,k;
     cin>>n>>k;
     vll v(n);
     scan_vector(v,n);
     while (k--)
     {
-      ll x;
-      cin>>x;
-      ll index=lower_bound(all(v),x+1)-v.begin();
-      cout<<index<<endl; 
-    }
+        ll x;
+        cin>>x;
+        //required index = (index of upper bound of x - 1) + 1 (1-indexing)
+        //               = index of upper bound of x
+        ll lo=0,hi=n-1;
+        while (hi-lo>1)
+        {
+            ll mid=(hi+lo)/2;
+            if (v[mid]<=x)
+                lo=mid+1;
+            else
+                hi=mid;
+        }
+        if (v[lo]>x)
+            cout<<lo<<endl;
+        else if (v[hi]>x)
+            cout<<hi<<endl;
+        else        //no upperbound
+            cout<<n<<endl;
+    }  
 }
 
 int main()
