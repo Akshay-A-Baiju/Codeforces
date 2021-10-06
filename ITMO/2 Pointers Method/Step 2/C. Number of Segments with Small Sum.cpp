38624c25e8db0077be3af57ed38b0ee542555601
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-10-06 20:20:00 +0530

    Create C. Number of Segments with Small Sum.cpp
    #2-pointers

---
+++ ITMO/2 Pointers Method/Step 2/C. Number of Segments with Small Sum.cpp
@@ -0,0 +1,104 @@
+// Author : Akshay A Baiju
+// Problem link : https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/C
+// Contest : CF EDU, 2 pointers, Step 2
+// Problem tags : 2 pointers
+// TC : O(n)
+// SC : O(n)
+
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define vi                  vector <int>
+#define vll                 vector <ll>
+#define vvll                vector <vll>
+#define pii                 pair <int,int>
+#define pll                 pair <ll,ll>
+#define vpll                vector <pll>
+#define ms(a,x)             memset (a,x,sizeof(a))
+#define rep(i,a,n)          for (int i=a;i<n;i++)
+#define rrep(i,n,a)         for (int i=n;i>=a;i--)
+#define pb                  push_back
+#define pf                  push_front
+#define ppb                 pop_back
+#define ff                  first
+#define ss                  second
+#define ins                 insert
+#define umap                unordered_map
+#define uset                unordered_set
+#define YY		              cout<<"YES"
+#define NN		              cout<<"NO"
+#define all(v)              v.begin(), v.end()
+#define desc()              greater <ll>()
+#define endl                "\n"   //not to be used in interactive problems
+#define scan_vector(v)      for (auto &it: v) { cin>>it; }
+#define print_vector(v)     for (auto &it: v) { cout<<it<<" "; } cout<<endl;
+#define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
+#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+// #define oset		          tree <ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> // find_by_order, order_of_key
+#ifndef ONLINE_JUDGE
+    #define debug(x)	    cerr<<#x<<" = "; _print(x); cerr<<"\n";
+#else
+    #define debug(x)
+#endif
+
+const int M = 1e9+7;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve()
+{
+    ll n,s;     cin>>n>>s;
+    vll v(n);   scan_vector(v);
+    //using 2 pointers
+    ll i=0,j=0,sum=0,count=0;
+    while (j<n)
+    {
+        sum+=v[j];
+        while (i<n && sum>s)
+        {
+            sum-=v[i];
+            i++;
+        }
+        count+=(j-i+1);
+        j++;
+    }
+    cout<<count;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        // freopen("input.txt","r",stdin);
+        // freopen("output.txt","w",stdout);
+        // freopen("error.txt","w",stderr);
+        clock_t clk = clock();
+    #endif
+    sync;
+    int t=1;
+    // cin>>t;
+    for (int test=1;test<=t;test++)
+    {
+        // cout<<"Case #"<<test<<": ";
+        solve();
+        cout<<endl;
+    }
+    #ifndef ONLINE_JUDGE
+        cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    #endif
+    return 0;
+}
