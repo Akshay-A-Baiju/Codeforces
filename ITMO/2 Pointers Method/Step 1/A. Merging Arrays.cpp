a7739b6da8d46474e909f7f413301f27cbd16a4d
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-10-06 19:45:04 +0530

    Update A. Merging Arrays.cpp

--- ITMO/2 Pointers Method/Step 1/A. Merging Arrays.cpp
+++ ITMO/2 Pointers Method/Step 1/A. Merging Arrays.cpp
@@ -1,11 +1,17 @@
 // Author : Akshay A Baiju
 // Problem link : https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
-// Problem tags : 2-pointer, easy
+// Contest : CF EDU - 2 pointers, Step 1
+// Problem tags : 2 pointers
 // TC : O(n+m)
 // SC : O(n+m)
 
 #include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+// using namespace __gnu_pbds;
+
 #define ll                  long long
 #define ull                 unsigned long long
 #define vi                  vector <int>
@@ -13,36 +19,58 @@ using namespace std;
 #define vvll                vector <vll>
 #define pii                 pair <int,int>
 #define pll                 pair <ll,ll>
+#define vpll                vector <pll>
 #define ms(a,x)             memset (a,x,sizeof(a))
 #define rep(i,a,n)          for (int i=a;i<n;i++)
 #define rrep(i,n,a)         for (int i=n;i>=a;i--)
 #define pb                  push_back
 #define pf                  push_front
 #define ppb                 pop_back
+#define ff                  first
+#define ss                  second
+#define ins                 insert
 #define umap                unordered_map
 #define uset                unordered_set
+#define YY		            cout<<"YES"
+#define NN		            cout<<"NO"
 #define all(v)              v.begin(), v.end()
 #define desc()              greater <ll>()
 #define endl                "\n"   //not to be used in interactive problems
-#define scan_vector(v,n)    for (auto &it: v) { cin>>it; }
+#define scan_vector(v)      for (auto &it: v) { cin>>it; }
 #define print_vector(v)     for (auto &it: v) { cout<<it<<" "; } cout<<endl;
 #define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
 #define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+// #define oset		        tree <ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> // find_by_order, order_of_key
+#ifndef ONLINE_JUDGE
+    #define debug(x)	    cerr<<#x<<" = "; _print(x); cerr<<"\n";
+#else
+    #define debug(x)
+#endif
+
 const int M = 1e9+7;
 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
 void solve()
-{   
-    //merge 2 arrays using 2-pointers  
-    ll n,m;
-    cin>>n>>m;
-    vll a(n),b(m),c(n+m);
-    scan_vector(a,n);
-    scan_vector(b,m);
-    ll i=0;     //iterate over a
-    ll j=0;     //iterate over b
-    while (i<n && j<m)
+{
+    ll n,m; cin>>n>>m;
+    vll a(n);   scan_vector(a);
+    vll b(m);   scan_vector(b);
+    vll c(n+m);
+    //using 2 pointers 
+    ll i=0,j=0;
+    while (i<n || j<m)
     {
-        if (a[i]<b[j])
+        if (j==m || (i<n && a[i]<=b[j]))
         {
             c[i+j]=a[i];
             i++;
@@ -53,31 +81,28 @@ void solve()
             j++;
         }
     }
-    //enter the rest array contents
-    while (i<n)
-    {
-        c[i+j]=a[i];
-        i++;
-    }
-    while (j<m)
-    {
-        c[i+j]=b[j];
-        j++;
-    }
     print_vector(c);
 }
 
 int main()
 {
-    clock_t clk = clock();
+    #ifndef ONLINE_JUDGE
+        // freopen("input.txt","r",stdin);
+        // freopen("output.txt","w",stdout);
+        // freopen("error.txt","w",stderr);
+        clock_t clk = clock();
+    #endif
     sync;
     int t=1;
-    //cin>>t;
-    while (t--)
+    // cin>>t;
+    for (int test=1;test<=t;test++)
     {
+        // cout<<"Case #"<<test<<": ";
         solve();
         cout<<endl;
     }
-    cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    #ifndef ONLINE_JUDGE
+        cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    #endif
     return 0;
 }
