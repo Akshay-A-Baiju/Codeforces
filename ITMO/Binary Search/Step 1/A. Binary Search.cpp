3cd0f2677166352023174aaae111c65e1e3ba19d
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-10-02 12:23:02 +0530

    Create A. Binary Search.cpp

---
+++ ITMO/Binary Search/Step 1/A. Binary Search.cpp
@@ -0,0 +1,54 @@
+// Author: Akshay A Baiju
+// Problem: A. Binary Search
+// URL: https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"   //not to be used in interactive problems
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define PI 3.141592653589793238462
+
+const int M = 1e9+7;
+const int MM = 998244353;
+const int N = 2e5+7;
+const ll inf = 1e18;
+
+void solve()
+{
+    ll n,k; cin>>n>>k;
+    vector <ll> v(n);
+    for (auto &it: v)
+        cin>>it;
+    while (k--)
+    {
+        ll x; cin>>x;
+        ll lo=0,hi=n-1;
+        while (hi-lo>1)
+        {
+            ll mid=(hi+lo)/2;
+            if (v[mid]<x)
+                lo=mid+1;
+            else
+                hi=mid;
+        }
+        if (v[lo]==x || v[hi]==x)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+}
+
+int main()
+{
+    sync;
+    int t=1;
+    // cin>>t;
+    for (int test=1;test<=t;test++)
+    {
+        solve();
+        cout<<endl;
+    }
+    return 0;
+}
